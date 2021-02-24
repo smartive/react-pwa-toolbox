904cb7f16e4cf234ac95fe493a6ddfcc9192678b
Mirco <9960444+mircostraessle@users.noreply.github.com>
2021-02-24 16:48:59 +0100

    feat: Manual periodic SW update (#8)
    BREAKING CHANGE: useServiceWorker hook now takes an object with two optional parameters

--- src/hooks/use-service-worker.ts
+++ src/hooks/use-service-worker.ts
@@ -4,29 +4,61 @@ import { Workbox } from 'workbox-window';
 let workbox: Workbox | null = null;
 let updateCallback: () => void = () => window.location.reload();
 
-export const useServiceWorker = (scriptUrl = 'service-worker.js'): [boolean, () => void | null] => {
+type Props = {
+  scriptUrl?: string;
+  periodicUpdateInterval?: number;
+};
+
+export const useServiceWorker = (props?: Props): [boolean, () => void | null] => {
   const [isServiceWorkerUpdateAvailable, setServiceWorkerUpdateAvailable] = useState(false);
 
   useEffect(() => {
-    if ('serviceWorker' in navigator && process.env.NODE_ENV !== 'development') {
-      workbox = new Workbox(scriptUrl);
+    let periodicUpdateHandler: NodeJS.Timeout | undefined;
 
-      workbox.addEventListener('waiting', () => setServiceWorkerUpdateAvailable(true));
+    // Inspired by https://developers.google.com/web/tools/workbox/guides/advanced-recipes#offer_a_page_reload_for_users
+    if ('serviceWorker' in navigator && process.env.NODE_ENV !== 'development') {
+      workbox = new Workbox(props?.scriptUrl || 'service-worker.js');
+      let registration: ServiceWorkerRegistration | undefined;
 
       updateCallback = () => {
         if (!workbox) {
           return;
         }
 
+        // Assuming the user accepted the update, set up a listener
+        // that will reload the page as soon as the previously waiting
+        // service worker has taken control.
         workbox.addEventListener('controlling', () => {
           window.location.reload();
         });
 
-        workbox.messageSW({ type: 'SKIP_WAITING' });
+        if (registration && registration.waiting) {
+          // Send a message to the waiting service worker,
+          // instructing it to activate.
+          workbox.messageSkipWaiting();
+        }
       };
 
-      workbox.register().then((registration) => !!registration.waiting && setServiceWorkerUpdateAvailable(true));
+      // Add an event listener to detect when the registered
+      // service worker has installed but is waiting to activate.
+      workbox.addEventListener('waiting', () => setServiceWorkerUpdateAvailable(true));
+
+      workbox.register().then((r) => {
+        registration = r;
+
+        // If a periodic update interval is set we manually check for service worker updates in this intervall
+        // (otherwise only the browser would check for updates e.g. on page navigations)
+        if (registration && props?.periodicUpdateInterval) {
+          periodicUpdateHandler = setInterval(() => registration?.update(), props.periodicUpdateInterval);
+        }
+      });
     }
+
+    return () => {
+      if (periodicUpdateHandler) {
+        clearInterval(periodicUpdateHandler);
+      }
+    };
   }, []);
 
   return [isServiceWorkerUpdateAvailable, updateCallback];
